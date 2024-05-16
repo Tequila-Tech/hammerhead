e43fee04158f452329510d5d378cf19b7cd416aa
Leonardo Lugo <leolugo5@outlook.com>
2024-05-16 13:26:55 -0500

    First launch

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import vercel from '@astrojs/vercel/serverless';
+
+import tunnel from "astro-tunnel";
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
+  integrations: [tailwind(), tunnel()]
+});
