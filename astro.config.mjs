c35c0390166f9e776a0414b68bec12d19abb485f
Leonardo Lugo <leolugo5@outlook.com>
2024-05-16 16:39:25 -0500

    new fix i hope this is it

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-
-import tunnel from "astro-tunnel";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), tunnel()]
-});
+  // your configuration options here...
+  // https://docs.astro.build/en/reference/configuration-reference/
+})
