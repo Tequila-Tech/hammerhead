ec46d068dd92a20adf2f9fd56bc304f1718a3bfa
Leonardo Lugo <leolugo5@outlook.com>
2024-05-20 22:09:50 -0500

    Adjusted the astro Config  MSJ for server and static build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,24 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
 import vercel from '@astrojs/vercel/serverless';
 
+import react from "@astrojs/react";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
   output: 'server',
   adapter: vercel(),
+  integrations: [react()]
 });
+
+// THIS IS TO EXPORT STATIC AND THE ABOVE TO EXPORT TO SERVER
+
+// import { defineConfig } from 'astro/config';
+// import tailwind from '@astrojs/tailwind';
+// import react from '@astrojs/react';
+// import vercel from '@astrojs/vercel/static';
+
+// export default defineConfig({
+//   integrations: [tailwind(), react()],
+//   output: 'static', // Choose either 'server' or 'static' depending on your deployment target
+//   adapter: vercel(),
+// });
