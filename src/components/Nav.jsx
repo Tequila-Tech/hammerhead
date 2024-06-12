c261ed9366a72584214748c754dce190dd763d1b
Leonardo Lugo <leolugo5@outlook.com>
2024-06-12 04:15:28 -0500

    Pretty much done

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -34,7 +34,7 @@ const NavBar = () => {
             Reviews
           </a>
         </li>
-        <li><a href="/blog">Blog</a></li>
+        <li><a href="https://blog.hammerheadcozumel.com/">Blog</a></li>
       </ul>
       <div className="cta">
         <a className="primary-cta" href="/booking">Book Now</a>
